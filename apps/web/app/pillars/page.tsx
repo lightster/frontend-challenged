81c8e78954806d394a231765d78e4565daab2427
Matt Light <matt@paren.io>
2023-05-13 22:36:50 -0700

    Fix linting issues

--- apps/web/app/pillars/page.tsx
+++ apps/web/app/pillars/page.tsx
@@ -6,7 +6,9 @@ function Pillars() {
   return (
     <div className={styles.container}>
       <div className={`${styles.pillar} ${styles.pillar1}`} />
-      <div className={`${styles.header} ${styles.header1}`}>Shadow &amp; Bone</div>
+      <div className={`${styles.header} ${styles.header1}`}>
+        Shadow &amp; Bone
+      </div>
       <div className={`${styles.description} ${styles.description1}`}>
         Dark forces conspire against orphan mapmaker Alina Starkov when she
         unleashes an extraordinary power that could change the fate of her
@@ -17,21 +19,25 @@ function Pillars() {
       <div className={`${styles.gap} ${styles.gap1}`} />
 
       <div className={`${styles.pillar} ${styles.pillar2}`} />
-      <div className={`${styles.header} ${styles.header2}`}>Better Call Saul</div>
+      <div className={`${styles.header} ${styles.header2}`}>
+        Better Call Saul
+      </div>
       <div className={`${styles.description} ${styles.description2}`}>
         The trials and tribulations of criminal lawyer Jimmy McGill in the years
-        leading up to his fateful run-in with Walter White and Jesse Pinkman.{" "}
+        leading up to his fateful run-in with Walter White and Jesse Pinkman.
       </div>
       <div className={`${styles.footer} ${styles.footer2}`}>AMC</div>
 
       <div className={`${styles.gap} ${styles.gap2}`} />
 
       <div className={`${styles.pillar} ${styles.pillar3}`} />
-      <div className={`${styles.header} ${styles.header3}`}>The Marvelous Mrs. Maisel</div>
+      <div className={`${styles.header} ${styles.header3}`}>
+        The Marvelous Mrs. Maisel
+      </div>
       <div className={`${styles.description} ${styles.description3}`}>
-        After her husband leaves her, young mother of two Miriam "Midge" Maisel
-        discovers that she has a talent for stand-up comedy. Could this be her
-        calling?
+        After her husband leaves her, young mother of two Miriam
+        &quote;Midge&quote; Maisel discovers that she has a talent for stand-up
+        comedy. Could this be her calling?
       </div>
       <div className={`${styles.footer} ${styles.footer3}`}>Amazon Prime</div>
     </div>
